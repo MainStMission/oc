080ef62245870930887a1572d3bc20e45a3f1136
tbrooke <tom.brooke@gmail.com>
2015-07-21 22:40:09 -0400

    fixed timeclock

--- app/controllers/time_clock_controller.rb
+++ app/controllers/time_clock_controller.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 class TimeClockController < ApplicationController
-  expose(:volunteers) { Volunteer.scoped.page params[:page] }
+  expose(:volunteers) { Volunteer.page params[:page] }
 end
